1af3e9793656e5c8500db20a5eb6d2f38bd74aeb
Abdul Rahman <abdulnsamba@gmail.com>
2024-05-09 12:22:37 +0300

    min ops

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""
+Min Opetaion module
+"""
+
+
+def minOperations(n):
+    """ Function cals the min"""
+
+    if n <= 0:
+        return 0
+    ops = 0
+    curlen = 1
+    clip = 0
+
+    while curlen < n:
+        if n % curlen == 0:
+            clip = curlen
+            ops += 1
+        curlen += clip
+        ops += 1
+    return ops
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,3 @@
+# 0x02-minimum_operations
+
+This question deals with calculating the number of opertaions needed to complete a specific task and in this case the task is copying and pasting values.
