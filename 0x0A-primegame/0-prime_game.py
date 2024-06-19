fb2354e9b69f89d18325ee491871a6aabaac9f7c
Abdul Rahman <abdulnsamba@gmail.com>
2024-06-19 22:00:38 +0300

    fix

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,43 +1,75 @@
 #!/usr/bin/python3
 """
-Prime Game Module
+  def isWinner(x, nums):
+      '''
+          determines the winner
+      '''
+      if not nums or x < 1:
+          return None
+      max_num = max(nums)
+
+      filter = [True for _ in range(max(max_num + 1, 2))]
+      for i in range(2, int(pow(max_num, 0.5)) + 1):
+          if not filter[i]:
+              continue
+          for j in range(i * i, max_num + 1, i):
+              filter[j] = False
+      filter[0] = filter[1] = False
+      y = 0
+      for i in range(len(filter)):
+          if filter[i]:
+              y += 1
+          filter[i] = y
+      player1 = 0
+      for x in nums:
+          player1 += filter[x] % 2 == 1
+      if player1 * 2 == len(nums):
+          return None
+      if player1 * 2 > len(nums):
+          return "Maria"
+      return "Ben"
 """
 
 
 def isWinner(x, nums):
     """
-    Prime Game Function
+    Determines the winner of a game based on a given set of numbers
+    and a threshold.
+
+    Args:
+        x (int): The threshold value.
+        nums (list): List of integers representing the numbers.
+
+    Returns:
+        str or None: The name of the winner (either "Maria" or "Ben"), or None
+        if no winner is determined.
     """
-    maria = 0
-    ben = 0
+    if not nums or x < 1:
+        return None
+    max_num = max(nums)
 
-    def SieveOfEratosthenes(n):
-        """Sieve of Eratosthenes Function"""
+    # Sieve of Eratosthenes to generate prime numbers
+    sieve = [True for _ in range(max(max_num + 1, 2))]
+    for i in range(2, int(pow(max_num, 0.5)) + 1):
+        if not sieve[i]:
+            continue
+        for j in range(i * i, max_num + 1, i):
+            sieve[j] = False
+    sieve[0] = sieve[1] = False
+    y = 0
+    for i in range(len(sieve)):
+        if sieve[i]:
+            y += 1
+        sieve[i] = y
 
-        prime = [True for i in range(n + 1)]
-        pointer = 2
-        while pointer * pointer <= n:
-            if prime[pointer] == True:
-                for i in range(pointer * pointer, n + 1, pointer):
-                    prime[i] = False
-            pointer += 1
-        prime[0] = False
-        prime[1] = False
-        return prime
+    # Counting the number of prime numbers for each player
+    player1 = 0
+    for x in nums:
+        player1 += sieve[x] % 2 == 1
 
-    max_n = max(nums)
-    primes = SieveOfEratosthenes(max_n)
-
-    for round in nums:
-        round_primes = [i for i in range(round + 1) if primes[i]]
-        if len(round_primes) % 2 == 0:
-            ben += 1
-        else:
-            maria += 1
-
-    if maria > ben:
+    # Determining the winner
+    if player1 * 2 == len(nums):
+        return None
+    if player1 * 2 > len(nums):
         return "Maria"
-    elif ben > maria:
-        return "Ben"
-
-    return None
+    return "Ben"
